3f00397e2bf5f8f5b6bc7a4d31d5166f7a2e0e4b
Vinícius Araújo <44041869+ViniciusHSAraujo@users.noreply.github.com>
2020-01-07 23:17:08 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using static System.Console;
+
+namespace Veiculo {
+    class Program {
+
+        public static Veiculo veiculo = null;
+        
+        static void Main(string[] args) {
+
+
+            while (veiculo == null) {
+                veiculo = CadastrarVeiculo();
+            }
+
+            int opcao = 0;
+            bool continuar = true;
+            do {
+                Clear();
+                Title = "Controle Veícular";
+                WriteLine("############## Painel de Controle do Veículo ##############");
+                WriteLine("Escolha uma das opções abaixo");
+                WriteLine("1 - Abastecer");
+                WriteLine("2 - Ligar ");
+                WriteLine("3 - Desligar");
+                WriteLine("4 - Acelerar");
+                WriteLine("5 - Frear");
+                WriteLine("6 - Mudar a cor");
+                WriteLine("0 - Sair");
+                var opcaoValida = false;
+                while (!opcaoValida) {
+                    try {
+                        opcao = Convert.ToInt32(ReadLine());
+                        opcaoValida = true;
+                    } catch (Exception) {
+                        WriteLine("Opção inválida!");
+                        opcaoValida = false;
+                    }
+                }
+
+                switch (opcao) {
+                    case 1:
+                        Clear();
+                        AbastecerVeiculo();
+                        break;
+                    case 2:
+                        Clear();
+                        LigarVeiculo();
+                        break;
+                    case 3:
+                        Clear();
+                        DesligarVeiculo();
+                        break;
+                    case 4:
+                        Clear();
+                        AcelerarVeiculo();
+                        break;
+                    case 5:
+                        Clear();
+                        FrearVeiculo();
+                        break;
+                    case 6:
+                        Clear();
+                        MudarACorDoVeiculo();
+                        break;
+                    case 0:
+                        continuar = false;
+                        break;
+                    default:
+                        WriteLine("Opção inválida!");
+                        break;
+                }
+
+            } while (continuar);
+        }
+
+        private static Veiculo CadastrarVeiculo() {
+            Title = "Cadastro do Veículo:";
+
+            WriteLine("Digite a marca do veículo");
+            string marca = ReadLine();
+
+            WriteLine("Digite o modelo do veículo");
+            string modelo = ReadLine();
+
+            WriteLine("Digite a placa do veículo");
+            string placa = ReadLine();
+
+            WriteLine("Digite a cor do veículo");
+            string cor = ReadLine();
+
+            WriteLine("Digite o preço do veículo");
+            double preco = Convert.ToDouble(ReadLine());
+
+            Veiculo veiculo;
+            try {
+                veiculo = new Veiculo(marca, modelo, placa, cor, preco);
+                WriteLine("Veículo cadastrado com sucesso");
+                WriteLine("Pressione qualquer tecla para voltar ao menu..");
+                ReadKey();
+                return veiculo;
+            } catch (Exception e) {
+                WriteLine($"Erro ao cadastrar: {e.Message}");
+                return null;
+            }
+        }
+
+        private static void AbastecerVeiculo() {
+            try {
+                WriteLine("Digite quantos litros você deseja abastecer:");
+                int qtde = Convert.ToInt32(ReadLine());
+
+                veiculo.Abastecer(qtde);
+
+                WriteLine($"Veículo abastecido com sucesso! Agora você está com {veiculo.LitrosCombustivel} litros de combustível.");
+
+            } catch (Exception e) {
+                WriteLine(e.Message);
+            } finally {
+                WriteLine("Pressione qualquer tecla para voltar ao menu..");
+                ReadKey();
+            }
+        }
+
+        private static void LigarVeiculo() {
+            try {
+                WriteLine("Ligando o carro..");
+                veiculo.Ligar();
+                WriteLine($"Veículo ligado com sucesso!");
+
+            } catch (Exception e) {
+                WriteLine(e.Message);
+            } finally {
+                WriteLine("Pressione qualquer tecla para voltar ao menu..");
+                ReadKey();
+            }
+        }
+
+        private static void DesligarVeiculo() {
+            try {
+                WriteLine("Desligando o carro..");
+                veiculo.Desligar();
+                WriteLine($"Veículo desligado com sucesso!");
+
+            } catch (Exception e) {
+                WriteLine(e.Message);
+            } finally {
+                WriteLine("Pressione qualquer tecla para voltar ao menu..");
+                ReadKey();
+            }
+        }
+
+        private static void AcelerarVeiculo() {
+            try {
+                WriteLine("Acelerando o carro..");
+                veiculo.Acelerar();
+                WriteLine($"Sua velocidade atual é {veiculo.Velocidade}!");
+
+            } catch (Exception e) {
+                WriteLine(e.Message);
+            } finally {
+                WriteLine("Pressione qualquer tecla para voltar ao menu..");
+                ReadKey();
+            }
+        }
+
+        private static void FrearVeiculo() {
+            try {
+                WriteLine("Freando o carro..");
+                veiculo.Frear();
+                WriteLine($"Sua velocidade atual é {veiculo.Velocidade}!");
+
+            } catch (Exception e) {
+                WriteLine(e.Message);
+            } finally {
+                WriteLine("Pressione qualquer tecla para voltar ao menu..");
+                ReadKey();
+            }
+        }
+
+        private static void MudarACorDoVeiculo() {
+            try {
+                WriteLine("Digite a nova cor do carro..");
+                var novaCor = ReadLine();
+                veiculo.Pintar(novaCor);
+                WriteLine($"Cor do carro alterada para {veiculo.Cor}!");
+
+            } catch (Exception e) {
+                WriteLine(e.Message);
+            } finally {
+                WriteLine("Pressione qualquer tecla para voltar ao menu..");
+                ReadKey();
+            }
+        }
+    }
+}
---
+++ Veiculo.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Veiculo {
+    class Veiculo {
+        public Veiculo(string marca, string modelo, string placa, string cor, double preco) {
+            Marca = marca;
+            Modelo = modelo;
+            Placa = placa;
+            Cor = cor;
+            Km = 0;
+            IsLigado = false;
+            LitrosCombustivel = 0;
+            Velocidade = 0;
+            Preco = preco;
+        }
+
+        public string Marca { get; set; }
+
+        public string Modelo { get; set; }
+
+        public string Placa { get; set; }
+
+        public string Cor { get; set; }
+
+        public float Km { get; set; }
+
+        public bool IsLigado { get; set; }
+
+        public int LitrosCombustivel { get; set; }
+
+        public int Velocidade { get; set; }
+
+        public double Preco { get; set; }
+
+        public void Acelerar() {
+            if (!IsLigado) {
+                throw new Exception("Não é possível acelerar com o carro desligado.");
+            }
+
+            if (LitrosCombustivel < 1) {
+                throw new Exception("Gasolina insuficiente! Abasteça o veículo para continuar acelerando.");
+            }
+
+            if (Velocidade >= 180) {
+                throw new Exception("Limite de velocidade atingido.");
+            }
+
+            LitrosCombustivel -= 1;
+            Velocidade += 20;
+        }
+
+        public void Abastecer(int qtde) {
+
+            if (IsLigado) {
+                throw new Exception("Não é possível abastecer com o carro ligado. Quer botar fogo no posto?");
+            }
+
+            if (LitrosCombustivel + qtde > 100) {
+                throw new Exception($"Não é possível abastecer essa quantidade, pois excede o limite do tanque de 100 litros. Quantidade máxima disponível = {100 - LitrosCombustivel}.");
+            }
+
+            LitrosCombustivel += qtde;
+        }
+
+        public void Frear() {
+            if (!IsLigado) {
+                throw new Exception("Não é possível frear com o carro desligado.");
+            }
+
+            if (Velocidade > 0) {
+                if (Velocidade - 10 < 0) {
+                    Velocidade = 0;
+                }
+                Velocidade -= 10;
+            } else {
+                throw new Exception("O carro já encontra-se parado!");
+            }
+
+        }
+
+        public void Pintar(string novaCor) {
+            Cor = novaCor;
+        }
+
+        public void Ligar() {
+            if (IsLigado) {
+                throw new Exception("O carro já encontra-se ligado.");
+            }
+
+            IsLigado = true;
+        }
+
+        public void Desligar() {
+            if (!IsLigado) {
+                throw new Exception("O carro já encontra-se desligado.");
+            }
+
+            IsLigado = false;
+        }
+    }
+}
